9827e65b735db0a38355fdb09009c84f4bc14946
Michael Thompson <micgtho@gmail.com>
2023-01-29 11:26:57 +0100

    quick python example

---
+++ geometry/main.py
@@ -0,0 +1,28 @@
+def calculate_area_rectangle(side, height):
+
+    # Implement function for calculating rectangle
+
+    return NotImplemented
+
+
+def calculate_square(side):
+
+    return side * side
+
+
+
+def main():
+
+    # calculate area of square
+    square_1 = calculate_square(4)
+    print(f"Area of square 1: {square_1}")
+
+    rectangle_1 = calculate_area_rectangle(2,4)
+    print(f"Area of rectangle 1: {rectangle_1}")
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ measurements/main.py
@@ -0,0 +1,24 @@
+def dm_to_meter(dm):
+
+    return NotImplemented
+
+def cm_to_meter(cm):
+
+    return cm / 100
+
+
+
+def main():
+
+    # calculate area of square
+    cm_calc = cm_to_meter(190)
+    print(f"Cm to meters: {cm_calc}")
+
+
+    dm_calc = cm_to_meter(18)
+    print(f"dm to meters: {dm_calc}")
+
+    
+
+if __name__ == '__main__':
+    main()
